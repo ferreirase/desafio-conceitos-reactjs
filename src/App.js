5657ecc9b7d6fa44caedead48794bea19ffac439
Anderson Raphael Ferreira <phaell.anderson09@gmail.com>
2020-05-22 13:43:15 -0300

    feature(project): project finish

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import api from './services/api'
+import { uuid } from 'uuidv4'
 
-import "./styles.css";
+import './styles.css'
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([])
+	const [id, setId] = useState(repositories.length)
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	useEffect(() => {
+		api.get('repositories').then((response) => setRepositories(response.data))
+		setId(repositories.length)
+	}, [repositories])
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	async function handleAddRepository() {
+		const newRepo = {
+			id: uuid(),
+			title: `Projeto adicionado ${id + 1}`,
+			url: 'https://github.com/ferreirase',
+			techs: ['Java', 'PHP'],
+		}
+		setId(id + 1)
+		const response = await api.post('/repositories', newRepo)
+		setRepositories([...repositories], response.data)
+	}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+	async function handleRemoveRepository(id) {
+		const repoDeleted = await api.delete(`repositories/${id}`)
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+		setRepositories(repositories.filter((repo) => repo.id !== repoDeleted.id))
+	}
+
+	return (
+		<>
+			<div>
+				<ul data-testid='repository-list'>
+					{repositories.map((repo) => (
+						<li key={repo.id}>
+							{repo.title}
+							<button onClick={() => handleRemoveRepository(repo.id)}>
+								Remover
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</>
+	)
 }
 
-export default App;
+export default App
